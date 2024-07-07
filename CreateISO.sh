464dc4aec2a032e200371a4cdca2216ea1c45dac
Logan Kissel <kissellogan10@gmail.com>
2024-07-07 18:14:39 -0400

    Added bash script compatibility

--- CreateISO.sh
+++ CreateISO.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/bash
+#! /usr/bin/env bash
 
 # Requires running InstallLimine.sh
 
--- InstallLimine.sh
+++ InstallLimine.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/bash
+#! /usr/bin/env bash
 # Download the latest Limine binary release for the 7.x branch.
 git clone https://github.com/limine-bootloader/limine.git --branch=v7.x-binary --depth=1
 
